2d278f41ef42d256e0a686dd6aa027241158b521
Arthur Benbassat <arthur.benbassat@outlook.com>
2019-05-07 11:36:46 +0200

    Done

--- leerlingopschool/Form1.Designer.vb
+++ leerlingopschool/Form1.Designer.vb
@@ -22,10 +22,104 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.txtSchool = New System.Windows.Forms.TextBox()
+        Me.txtKlas = New System.Windows.Forms.TextBox()
+        Me.txtNaam = New System.Windows.Forms.TextBox()
+        Me.BindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtSchool
+        '
+        Me.txtSchool.Location = New System.Drawing.Point(141, 62)
+        Me.txtSchool.MaxLength = 30
+        Me.txtSchool.Name = "txtSchool"
+        Me.txtSchool.Size = New System.Drawing.Size(174, 20)
+        Me.txtSchool.TabIndex = 0
+        '
+        'txtKlas
+        '
+        Me.txtKlas.Location = New System.Drawing.Point(141, 124)
+        Me.txtKlas.MaxLength = 4
+        Me.txtKlas.Name = "txtKlas"
+        Me.txtKlas.Size = New System.Drawing.Size(174, 20)
+        Me.txtKlas.TabIndex = 1
+        '
+        'txtNaam
+        '
+        Me.txtNaam.Location = New System.Drawing.Point(141, 191)
+        Me.txtNaam.MaxLength = 60
+        Me.txtNaam.Name = "txtNaam"
+        Me.txtNaam.Size = New System.Drawing.Size(174, 20)
+        Me.txtNaam.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(42, 62)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "School"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(42, 124)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(27, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Klas"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(42, 191)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Naam"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(141, 287)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(158, 63)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Toevoegen"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtNaam)
+        Me.Controls.Add(Me.txtKlas)
+        Me.Controls.Add(Me.txtSchool)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents txtSchool As TextBox
+    Friend WithEvents txtKlas As TextBox
+    Friend WithEvents txtNaam As TextBox
+    Friend WithEvents BindingSource1 As BindingSource
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
 End Class
--- leerlingopschool/Form1.vb
+++ leerlingopschool/Form1.vb
@@ -1,3 +1,14 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim naam, school, klas As String
+        Dim lijn As String
 
+        FileOpen(1, "C:\Users\Arthur\Downloads\schoolgegevens.txt", OpenMode.Append)
+
+        lijn = txtSchool.Text.PadRight(30) & txtKlas.Text.PadRight(4) & txtNaam.Text
+
+        PrintLine(1, lijn)
+        FileClose(1)
+
+    End Sub
 End Class
